8e1216c1f31d5961b5d5f985c36f47e35928a4ab
Victor Hugo da Silva Dias <victorhugo.dias@uol.com.br>
2017-11-27 16:10:56 -0200

    Testing tag to not render, virtual button and physics
    The script "Controle_Rastreio_Marcador" not work with "Virtual Button Controler" but separately works.

---
+++ Assets/Scripts/VirtualButtonController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class VirtualButtonController : MonoBehaviour, IVirtualButtonEventHandler {
+    private GameObject Cubo;
+    // Use this for initialization
+	void Start () {
+        // Gera uma lista dos filhos que são botões virtuais
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+
+        for (int i = 0; i < vbs.Length; i++)
+        {
+            //Registra os botões para que eventos possam ser acionados
+            vbs[i].RegisterEventHandler(this);
+        }
+
+        Cubo = transform.Find("Cube").gameObject;
+	}
+
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        switch (vb.VirtualButtonName)
+        {
+            case "Troca cor":
+                Renderer renderer = Cubo.GetComponent<Renderer>();
+                renderer.material.SetColor("_Color", Color.blue);
+                break;
+        }
+    }
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        switch (vb.VirtualButtonName)
+        {
+            case "Troca cor":
+                Renderer renderer = Cubo.GetComponent<Renderer>();
+                renderer.material.SetColor("_Color", Color.red);
+                break;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
