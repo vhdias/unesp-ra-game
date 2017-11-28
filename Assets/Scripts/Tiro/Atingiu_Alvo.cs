892a0ff2bd5cf41050732e4645261b21bd0ab594
João Pedro <jotamucheroni@gmail.com>
2017-11-28 14:40:21 -0200

    Scripts to firing

---
+++ Assets/Scripts/Tiro/Atingiu_Alvo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Atingiu_Alvo : MonoBehaviour {
+
+    public static int pontos = 0;
+
+	// Use this for initialization
+	void Start () {
+        pontos = 0;
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Bola(Clone)")
+        {
+            pontos = pontos + 1;
+
+            Destroy (this.gameObject, 0.03f);
+            Destroy(col.gameObject, 0.05f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Tiro/GUI_Trigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUI_Trigger : MonoBehaviour {
+
+    public static int score;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Text>().text = "BEM VINDO. \nATINJA TODOS OS BLOCOS";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       /* score = Atigiu_Alvo.pontos;
+
+        if(score == 9)
+        {
+            this.GetComponent<Text>().text = "PARABENS, VOCE ATINGIU TODOS OS CUBOS!";
+        }
+        else
+            if (score != 0 && score <= 9)
+                this.GetComponent<Text>().text = "BLOCOS ATINGIDOS = " + score;*/
+    }
+}
