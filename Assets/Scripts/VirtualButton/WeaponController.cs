878a3d68e1c5d014857ceb87dff5a2bb214f8a9c
Victor Hugo da Silva Dias <victorhugo.dias@uol.com.br>
2017-11-30 17:39:42 -0200

    Changing sphere to grenade on shoot
    Changed script WeaponController to add a aleatory rotation.
    Changed script ActionRA to return the generated Rigidbody on Atira function.

--- Assets/Scripts/VirtualButton/WeaponController.cs
+++ Assets/Scripts/VirtualButton/WeaponController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class WeaponController : DefaultVirtualButtonController {
     public Rigidbody bullet;
+    public float speed;
 
     override protected void ButtonPressed()
     {
-        ActionRA.Atira(new Vector3(0, -1, 0), transform, bullet, 10);
+        ActionRA.Atira(new Vector3(0, -1, 0), transform, bullet, speed).AddRelativeTorque(Random.rotation * new Vector3(1,1,1));
     }
 }
