0390183dfd729683ef28dc52665ec9e3b2a7561b
Victor Hugo da Silva Dias <victorhugo.dias@uol.com.br>
2017-12-02 20:40:04 -0200

    Correcting AtingiuAlvo script

--- Assets/Scripts/AtingiuAlvo.cs
+++ Assets/Scripts/AtingiuAlvo.cs
@@ -3,26 +3,18 @@ using UnityEngine;
 
 public class AtingiuAlvo : MonoBehaviour {
     public float timeToDestroy = 0;
-    
+
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name == "EsferaParticulas")
+        if (col.gameObject.name == "Grenade(Clone)")
         {
             Destroy(gameObject, timeToDestroy);
-            Debug.Log("Colisão com a explosão");
-        } else if (col.gameObject.name == "target Sphere")
-        {
-            Destroy(gameObject, timeToDestroy);
-            Debug.Log("Colisão com o raio");
+            //Debug.Log("Colisão com a explosão");
         }
-    }
-    private void OnParticleCollision(GameObject other)
-    {
-        var rigid = other.transform.parent.GetComponentInChildren<Rigidbody>();
-        //other.transform.rotation = transform.rotation;
-        //other.transform.position = transform.position;
-        rigid.velocity = Vector3.zero;
-        rigid.angularVelocity = Vector3.zero;
-        Debug.Log("Colisão de particulas");
+        else if(col.gameObject.name == "Lightning(Clone)")
+        {
+            Destroy(gameObject, timeToDestroy);
+            //Debug.Log("Colisão com a esfera de raio");
+        }
     }
 }
