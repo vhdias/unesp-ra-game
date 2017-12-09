5fb4e8504a744136c2a0d1678870b597809821ca
Victor Hugo da Silva Dias <victorhugo.dias@uol.com.br>
2017-12-09 18:57:12 -0200

    Create script to rotate objects
    The rotation occours in fixed update

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public Vector3 rotation;
+    public float velocidade = 1;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(rotation * velocidade);
+    }
+}
