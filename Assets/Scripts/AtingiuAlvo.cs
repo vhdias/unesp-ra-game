7e6c17d5ca45ae031713732c4c6c902823c92b09
Victor Hugo da Silva Dias <victorhugo.dias@uol.com.br>
2017-12-09 22:39:55 -0200

    Create a LevelController and add to scene

--- Assets/Scripts/AtingiuAlvo.cs
+++ Assets/Scripts/AtingiuAlvo.cs
@@ -2,19 +2,36 @@
 using UnityEngine;
 
 public class AtingiuAlvo : MonoBehaviour {
-    public float timeToDestroy = 0;
+    public float timeToDestroy = 1 , scale = 1.5f;
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.name == "EsferaParticulas")
         {
-            Destroy(gameObject, timeToDestroy);
+            ActivateRigidbodyGravity();
+            Destroy(gameObject, timeToDestroy * scale);
             //Debug.Log("Colisão com a explosão");
         }
         else if(col.gameObject.name == "Lightning(Clone)")
         {
+            ActivateRigidbodyGravity();
             Destroy(gameObject, timeToDestroy);
             //Debug.Log("Colisão com a esfera de raio");
         }
+        else if(col.gameObject.name.Contains("Lata")){
+            ActivateRigidbodyGravity();
+            //Debug.Log("Colisão entre latas");
+        } else if(col.gameObject.name == "Chao")
+        {
+            ActivateRigidbodyGravity();
+            Destroy(gameObject, timeToDestroy * scale * 3);
+        }
+    }
+
+    void ActivateRigidbodyGravity()
+    {
+        var rigid = GetComponent<Rigidbody>();
+        rigid.isKinematic = false;
+        rigid.useGravity = true;
     }
 }
